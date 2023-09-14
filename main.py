c826b185af8cc97a0d777aa10dfd7f32973b77fd
Kevin Maurizi <kevin.maurizi@bzz.ch>
2023-09-14 14:41:35 +0200

    done

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 def filter_students_by_grade(students):
-    # TODO: Nutze eine List Comprehension, um Studenten zu filtern, deren Note kleiner als 4.0 ist.
+    """
+    Nutze eine List Comprehension, um Studenten zu filtern, deren Note kleiner als 4.0 ist.
+
+    Parameters:
+    - students (list): Liste der Studenten und ihrer Noten.
+
+    Returns:
+    - list: Liste der gefilterten Studenten.
+    """
     return [student for student in students if student[1] < 4.0]
     # oder: return [(name, grade) for name, grade in students if grade < 4.0]
 
