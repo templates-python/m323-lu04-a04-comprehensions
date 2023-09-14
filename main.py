3e6833a3908fafe6ca0051c3007211fa1b5d97c3
Kevin Maurizi <kevin.maurizi@bzz.ch>
2023-09-14 12:08:12 +0200

    done

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-def calculate():
-    """
-    calculates the total
-    :return: None
-    """
-    price = 15.75
-    quantity = 3  # TODO change quantity to 5
-    print(f'Total: {price * quantity}')
+def filter_students_by_grade(students):
+    # TODO: Nutze eine List Comprehension, um Studenten zu filtern, deren Note kleiner als 4.0 ist.
+    return []
 
 
 if __name__ == '__main__':
-    calculate()
+    students = [
+        ('Alice', 4.0),
+        ('Bob', 3.5),
+        ('Charlie', 4.25),
+        ('David', 5.5),
+        ('Manuel', 3.75)
+    ]
+    result = filter_students_by_grade(students)
+    print(result)
--- main_test.py
+++ main_test.py
@@ -1,6 +1,16 @@
-import main
+from main import filter_students_by_grade
 
-def test(capsys):
-    main.calculate()
-    captured = capsys.readouterr()
-    assert captured.out == "Total: 78.75\n"
+
+def test_filter_students_by_grade():
+    students = [
+        ('Alice', 4.0),
+        ('Bob', 3.5),
+        ('Charlie', 4.25),
+        ('David', 5.5),
+        ('Manuel', 3.75)
+    ]
+    result = filter_students_by_grade(students)
+
+    # Überprüfen, ob nur die Studenten mit Noten kleiner als 4.0 zurückgegeben werden
+    assert len(result) == 1
+    assert result[0] == ('Bob', 3.5)
