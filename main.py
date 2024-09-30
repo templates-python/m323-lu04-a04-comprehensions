099c9a4b832174ab81eb7a96d26b4f70e81d7330
Kevin Maurizi <kevin.maurizi@bzz.ch>
2024-09-30 09:05:00 +0200

    Ran pyink on branch solution

--- main.py
+++ main.py
@@ -11,13 +11,14 @@ def filter_students_by_grade(students):
     return [student for student in students if student[1] < 4.0]
     # oder: return [(name, grade) for name, grade in students if grade < 4.0]
 
+
 if __name__ == '__main__':
     students = [
         ('Alice', 4.0),
         ('Bob', 3.5),
         ('Charlie', 4.25),
         ('David', 5.5),
-        ('Manuel', 3.75)
+        ('Manuel', 3.75),
     ]
     result = filter_students_by_grade(students)
     print(result)
